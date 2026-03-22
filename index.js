e4598c1cf08d604221496e23963a42ae3057e6a7
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-03-22 17:55:05 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,34 @@
+const { StacksTestnet, StacksMainnet } = require('@stacks/network');
+const { broadcastTransaction, makeSTXTokenTransfer } = require('@stacks/transactions');
+const { validateStacksAddress } = require('@stacks/crypto');
+
+function stxToMicro(amount) {
+  return BigInt(Math.round(amount * 1e6));
+}
+
+function microToStx(amount) {
+  return Number(amount) / 1e6;
+}
+
+function isValidAddress(address) {
+  return validateStacksAddress(address);
+}
+
+async function sendSTX(senderKey, recipient, amount, network='testnet') {
+  const net = network === 'mainnet' ? new StacksMainnet() : new StacksTestnet();
+  const txOptions = {
+    recipient,
+    amount: stxToMicro(amount),
+    senderKey,
+    network: net,
+  };
+  const tx = await makeSTXTokenTransfer(txOptions);
+  return await broadcastTransaction(tx, net);
+}
+
+module.exports = {
+  stxToMicro,
+  microToStx,
+  isValidAddress,
+  sendSTX
+};
