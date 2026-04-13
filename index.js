23181111812bb73968f637931dd05706024ff253
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-04-13 10:33:31 +0100

    Update index.js

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ export function isValidAddress(address) {
 // Send STX using a private key (Designed for bckend/Nde.js usage)
 export async function sendSTX(senderKey, recipient, amount, network = 'testnet') {
   if (!senderKey || !recipient || !amount) {
-    throw new Error("Missing required parametes (senderKey, recipient, amount) for sendSTX");
+    throw new Error("Missing required parametes (senderKe, recipient, amount) for sendSTX");
   }
 
   const net = network === 'mainnet' ? new StacksMainnet) : new StacksTestnet();
