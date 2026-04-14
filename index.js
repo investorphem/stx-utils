e62d43023c772f6cd33681e81ed2568afc377d68
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-04-14 08:19:47 +0100

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ export function microToStx(amount) {
 
 // Validate a Stacks address
 export function isValidAddress(address) {
-  if (!address || typeof address !== 'string') return false;
+  if (!address || typeof address!== 'string') return false;
   try {
     return validateStacksAddress(address);
   } catch 
