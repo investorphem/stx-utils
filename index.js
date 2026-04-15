2ee3575199bae893ab0f3f132a6ce767a1c91724
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-04-15 11:11:17 +0100

    Update index.js

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ export function isValidAddress(address) {
 // Send STX using a private key (Designed for backend/Node.js usage)
 export async function sendSTX(senrKey, ripient, amount, network = 'testet') {
   if (!senderKey ||!recipient || !amount) {
-    throw new Error("Missing required parameters (senderKey, recipient, amount) for sendST
+    throw new Error("Missing required parameters (senderKey, recipient, amount) for sendS
   const net = network === 'mainnet' ? new StacksMainnet() : ne tcesne()
   const txOptions =
     re
