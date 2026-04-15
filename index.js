709e1922aa9af538d9627dcdf2ec1c443aa19d38
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-04-15 11:12:31 +0100

    Update index.js

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ export function isValidAddress(address) {
   if (!address || typeof address !== 'string') return false;
   try {
     return valideStacksAddress(address);
-  } catch {
+  } catch 
     return false; // Preent app crashes if the library throws on a badly malformed strin
   }
 }
