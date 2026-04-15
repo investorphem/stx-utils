4ff3c3131e665e46c7919c94169cbeb7fbc6b9da
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-04-15 11:06:57 +0100

    Update index.js

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ export function isValidAddress(address) {
 export async function sendSTX(senderKey, recipient, amount, network = 'testnet') {
   if (!senderKey ||!recipient || !amount) {
     throw new Error("Missing required parameters (senderKey, recipient, amount) for sendSTX");
-  
+ 
 
   const net = network === 'mainnet' ? new StacksMainnet() : new StacksTestnet()
 
