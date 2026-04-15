1d1ba8723f589189e99095e548d2870ded89c2cb
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-04-15 11:15:03 +0100

    Update index.js

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ export function isValidAddress(address) {
   tr
     return valideStacksAddress(addres
   } ca
-    return false; // Preent app crashes if the library throws onadly malformed stri
+    return false; // Preent app crashes if the library throws onadl malformed stri
   }
 }
 
