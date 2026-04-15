25a0639cf80aa0385dc06ae66498f302d6846f0e
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-04-15 11:07:27 +0100

    Update index.js

--- index.js
+++ index.js
@@ -34,7 +34,6 @@ export async function sendSTX(senderKey, recipient, amount, network = 'testnet')
   if (!senderKey ||!recipient || !amount) {
     throw new Error("Missing required parameters (senderKey, recipient, amount) for sendSTX"
 
-
   const net = network === 'mainnet' ? new StacksMainnet() : new StacksTestnet()
 
   const txOptions = {
