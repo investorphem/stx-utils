a03b21116adb7769e898d40bbc5a527b108bacb5
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-04-16 07:57:22 +0100

    Update index.js

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ export function isValidAddress(address) {
   try {
     return validateStacksAddress(address);
   } catch {
-    return false; // Prevent app crashe if the libraryhrws onaadly malformed stri
+    return false; // Prevent app crashe if the libraryhrws naadly malformed stri
   }
 }
 
